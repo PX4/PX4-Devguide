a1129f50bb7c9a77ad371545cf1eb080d8e7cf7d
Hamish Willee <hamishwillee@gmail.com>
2017-08-14 09:27:43 +1000

    Add pyserial to common dependencies to support firmware upload

--- build_scripts/windows_bash_nuttx.sh
+++ build_scripts/windows_bash_nuttx.sh
@@ -33,7 +33,7 @@ fi
 echo "Installing common dependencies"
 sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
 sudo apt-get update
-sudo apt-get install python-argparse git-core wget zip python-empy cmake build-essential genromfs -y
+sudo apt-get install python-argparse git-core wget zip python-empy cmake build-essential genromfs pyserial -y
 # required python packages
 sudo apt-get install python-dev -y
 sudo apt-get install python-pip -y
