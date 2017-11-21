b621e303f02da7d1f59d7c8015b6e24264c6b6d9
Vangelis Tasoulas <vangelis@tasoulas.net>
2017-11-21 11:44:27 +0100

    Remove duplicate code from the ubuntu build scripts.
    All the scripts are installing at least the common
    dependencies, but the shell code that installs the common
    dependencies was copied in each one of the scripts. This is
    error prone. If the common dependencies change, then one
    has to go to each one of these scripts to do the changes.
    
    This commit changes that behavior and make all the ubuntu
    scripts depend on another script that is keeping the common
    dependencies. Keep on reading for more details.
    
    The logical thing to do is that the scripts `ubuntu_sim.sh`,
    `ubuntu_sim_ros_gazebo.sh` and `ubuntu_sim_nuttx.sh` should
    depend on another script that is responsible to install the
    common dependencies, and install on top of the common deps
    the additional components that needs to be installed for
    each specific script. This script is now called
    `ubuntu_sim_ros_gazebo.sh`.
    
    How it works?
    When a user downloads e.g. the script `ubuntu_sim_ros_gazebo.sh`,
    the first thing this script does is to download and source the
    script called `ubuntu_sim_common_deps.sh` before installing the
    specific ROS/Gazebo stuff.
    
    The script `ubuntu_sim_common_deps.sh` is now the only
    script that we have to maintain when the common dependencies
    change.
    
    Signed-off-by: Vangelis Tasoulas <vangelis@tasoulas.net>

--- build_scripts/ubuntu_sim_ros_gazebo.sh
+++ build_scripts/ubuntu_sim_ros_gazebo.sh
@@ -4,76 +4,17 @@
 ## It installs the common dependencies for all targets (including Qt Creator) and the ROS Kinetic/Gazebo 7 (the default).
 ##
 ## Installs:
-## - Common dependencies and tools for all targets (including: Ninja build system, Qt Creator, pyulog)
-## - FastRTPS and FastCDR
+## - Common dependencies libraries and tools as defined in `ubuntu_sim_common_deps.sh`
 ## - ROS Kinetic (including Gazebo7)
 ## - MAVROS
-## - PX4/Firmware source (to ~/src/Firmware/)
 
-# Preventing sudo timeout https://serverfault.com/a/833888
-trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
-
-
-# Ubuntu Config
-sudo apt-get remove modemmanager -y
-
-# Ninja build system
-ninja_dir=$HOME/ninja
-echo "Installing Ninja to: $ninja_dir."
-if [ -d "$ninja_dir" ]
-then
-    echo " Ninja already installed."
-else
-    pushd .
-    mkdir -p $ninja_dir
-    cd $ninja_dir
-    wget https://github.com/martine/ninja/releases/download/v1.6.0/ninja-linux.zip
-    unzip ninja-linux.zip
-    rm ninja-linux.zip
-    exportline="export PATH=$ninja_dir:\$PATH"
-    if grep -Fxq "$exportline" ~/.profile; then echo " Ninja already in path" ; else echo $exportline >> ~/.profile; fi
-    . ~/.profile
-    popd
-fi
-
-# Common dependencies
-echo "Installing common dependencies"
-sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
-sudo apt-get update -y
-sudo apt-get install python-argparse git git-core zip python-empy python-toml python-numpy qtcreator cmake build-essential genromfs -y
-# Required python packages
-sudo apt-get install python-dev -y
-sudo apt-get install python-pip -y
-sudo -H pip install --upgrade pip
-sudo -H pip install pandas jinja2
-pip install pyserial
-# optional python tools
-pip install pyulog
-
-# Install FastRTPS 1.5.0 and FastCDR-1.0.7
-fastrtps_dir=$HOME/eProsima_FastRTPS-1.5.0-Linux
-echo "Installing FastRTPS to: $fastrtps_dir"
-if [ -d "$fastrtps_dir" ]
-then
-    echo " FastRTPS already installed."
-else
-    pushd .
-    cd ~
-    wget http://www.eprosima.com/index.php/component/ars/repository/eprosima-fast-rtps/eprosima-fast-rtps-1-5-0/eprosima_fastrtps-1-5-0-linux-tar-gz -O eprosima_fastrtps-1-5-0-linux.tar.gz
-    tar -xzf eprosima_fastrtps-1-5-0-linux.tar.gz eProsima_FastRTPS-1.5.0-Linux/
-    tar -xzf eprosima_fastrtps-1-5-0-linux.tar.gz requiredcomponents
-    tar -xzf requiredcomponents/eProsima_FastCDR-1.0.7-Linux.tar.gz
-    cpucores=$(( $(lscpu | grep Core.*per.*socket | awk -F: '{print $2}') * $(lscpu | grep Socket\(s\) | awk -F: '{print $2}') ))
-    cd eProsima_FastCDR-1.0.7-Linux; ./configure --libdir=/usr/lib; make -j$cpucores; sudo make install
-    cd ..
-    cd eProsima_FastRTPS-1.5.0-Linux; ./configure --libdir=/usr/lib; make -j$cpucores; sudo make install
-    cd ..
-    rm -rf requiredcomponents eprosima_fastrtps-1-5-0-linux.tar.gz
-    popd
-fi
-
-# jMAVSim simulator dependencies
-sudo apt-get install ant openjdk-8-jdk openjdk-8-jre -y
+# Source the ubuntu_sim_common_deps.sh script directly from github
+common_deps=$(wget https://raw.githubusercontent.com/PX4/Devguide/master/build_scripts/ubuntu_sim_common_deps.sh -O -)
+wget_return_code=$?
+# If there was an error downloading the dependent script, we must warn the user and exit at this point.
+if [[ $wget_return_code -ne 0 ]]; then echo "Error downloading 'ubuntu_sim_common_deps.sh'"; exit 1; fi
+# Otherwise source the downloaded script.
+. <(echo "${common_deps}")
 
 # ROS Kinetic/Gazebo (ROS Kinetic includes Gazebo7 by default)
 ## Gazebo simulator dependencies
@@ -130,18 +71,7 @@ if grep -Fxq "$catkin_ws_source" ~/.bashrc; then echo ROS catkin_ws setup.bash a
 else echo "$catkin_ws_source" >> ~/.bashrc; fi
 eval $catkin_ws_source
 
-# Clone PX4/Firmware
-clone_dir=~/src
-echo "Cloning PX4 to: $clone_dir."
-if [ -d "$clone_dir" ]
-then
-    echo " Firmware already cloned."
-else
-    mkdir -p $clone_dir
-    cd $clone_dir
-    git clone https://github.com/PX4/Firmware.git
-    cd Firmware
-fi
+# Go to the firmware directory
 cd $clone_dir/Firmware
 
 if [[ ! -z $unsupported_os ]]; then
