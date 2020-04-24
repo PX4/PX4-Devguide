873f8c6c1b02307e083128f90803a02e03cc1438
Pedro Roque <padr@kth.se>
2020-04-24 16:48:32 +0200

    Removed unneeded FastRTPS depends.

--- build_scripts/ubuntu_sim_common_deps.sh
+++ build_scripts/ubuntu_sim_common_deps.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-## Bash script for setting up a PX4 development environment on Ubuntu LTS (16.04).
 ## It can be used for installing simulators (only) or for installing the preconditions for Snapdragon Flight or Raspberry Pi.
 ##
 ## Installs:
@@ -30,26 +29,6 @@ sudo -H pip install --upgrade pip
 sudo -H pip install pandas jinja2 pyserial pyyaml
 # optional python tools
 sudo -H pip install pyulog
-
-# Install FastRTPS 1.7.1 and FastCDR-1.0.8
-fastrtps_dir=$HOME/eProsima_FastRTPS-1.7.1-Linux
-echo "Installing FastRTPS to: $fastrtps_dir"
-if [ -d "$fastrtps_dir" ]
-then
-    echo " FastRTPS already installed."
-else
-    pushd .
-    cd ~
-    wget https://www.eprosima.com/index.php/component/ars/repository/eprosima-fast-rtps/eprosima-fast-rtps-1-7-1/eprosima_fastrtps-1-7-1-linux-tar-gz -O eprosima_fastrtps-1-7-1-linux.tar.gz
-    tar -xzf eprosima_fastrtps-1-7-1-linux.tar.gz eProsima_FastRTPS-1.7.1-Linux/
-    tar -xzf eprosima_fastrtps-1-7-1-linux.tar.gz requiredcomponents
-    tar -xzf requiredcomponents/eProsima_FastCDR-1.0.8-Linux.tar.gz
-    cpucores=$(( $(lscpu | grep Core.*per.*socket | awk -F: '{print $2}') * $(lscpu | grep Socket\(s\) | awk -F: '{print $2}') ))
-    (cd eProsima_FastCDR-1.0.8-Linux && ./configure --libdir=/usr/lib && make -j$cpucores && sudo make install)
-    (cd eProsima_FastRTPS-1.7.1-Linux && ./configure --libdir=/usr/lib && make -j$cpucores && sudo make install)
-    rm -rf requiredcomponents eprosima_fastrtps-1-7-1-linux.tar.gz
-    popd
-fi
 
 # jMAVSim simulator dependencies
 echo "Installing jMAVSim simulator dependencies"
